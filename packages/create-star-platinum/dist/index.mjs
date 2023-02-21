dea9ba3021d69e7460c5641a6a4e65353043974c
qi-chen-ming <15810420185@163.com>
2023-02-22 05:49:21 +0800

    feat(create-star-platinum): 完成了vue和react的普通模板引用

---
+++ packages/create-star-platinum/dist/index.mjs
@@ -0,0 +1,6 @@
+import jiti from "file:///D:/web-%E9%BD%90%E6%99%A8%E6%98%8E/%E8%84%9A%E6%89%8B%E6%9E%B6%E9%A1%B9%E7%9B%AE/star-platinum-cli/packages/create-star-platinum/node_modules/.pnpm/registry.npmmirror.com+jiti@1.17.1/node_modules/jiti/lib/index.js";
+
+/** @type {import("D:/web-齐晨明/脚手架项目/star-platinum-cli/packages/create-star-platinum/src/index")} */
+const _module = jiti(null, { interopDefault: true, esmResolve: true })("D:/web-齐晨明/脚手架项目/star-platinum-cli/packages/create-star-platinum/src/index.ts");
+
+export default _module;
